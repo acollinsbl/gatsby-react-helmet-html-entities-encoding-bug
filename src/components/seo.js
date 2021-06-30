6d917c8287b1978fae6cb75a38cb72e804541836
Anthony Collins <a.r.c_@hotmail.co.uk>
2021-06-30 08:08:45 +0100

    setup reproduction for react helmet bug

---
+++ src/components/seo.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Helmet } from 'react-helmet';
+
+const SEO = () => {
+    return(
+        <Helmet encodeSpecialCharacters={false} title={'Testing & Testing'}></Helmet>
+    )
+};
+
+export default SEO;
